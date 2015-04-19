1a84252f82641bb75f646256322952f2cc5fbc82
JK15825 <baseballranger@gmail.com>
2015-04-18 21:42:31 -0500

    Ehhh??!?!?

--- Pantrie/app/src/main/java/com/nicodangelo/pantrie/item/ListAdapter.java
+++ Pantrie/app/src/main/java/com/nicodangelo/pantrie/item/ListAdapter.java
@@ -1,40 +1,37 @@
 package com.nicodangelo.pantrie.item;
 
-import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import com.nicodangelo.pantrie.R;
 
-public class ListAdapter extends ActionBarActivity {
+import java.util.ArrayList;
 
+public class ListAdapter extends ArrayAdapter<String>
+{
+    private Context context;
+    private final ArrayList<String> values;
+    public ListAdapter(Context context, ArrayList<String> values)
+    {
+        super(context, R.layout.activity_list_adapter,values);
+        this.context = context;
+        this.values = values;
+    }
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list_adapter);
+    public View getView(int position, View convertView, ViewGroup parent)
+    {
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View rowView = inflater.inflate(R.layout.activity_list_adapter, parent, false);
+        TextView itemNameTextView = (TextView)  rowView.findViewById(R.id.itemNameTextView);
+        itemNameTextView.setText(values.get(position));
+        TextView itemAmountTextView = (TextView) rowView.findViewById(R.id.itemAmountTextView);
+        itemAmountTextView.setText(values.get(position));
+        return rowView;
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_list_adapter, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
